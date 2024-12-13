500d440e1f8e20fe9a3b31c33355d7f8c8d6b246
Play3rgg <39414270+Play3rgg@users.noreply.github.com>
2024-12-13 09:54:50 +0200

    Added templates
    Shout out to skill!

--- a3ae_pracs/addons/templates/Templates/Templates.hpp
+++ a3ae_pracs/addons/templates/Templates/Templates.hpp
@@ -1,19 +1,46 @@
 class Templates {
-    class Vanilla_Base; //import Vanilla_Base from A3A to use with defining a new vanilla template
 
-    class Vanilla_AAF { // overwrite existing template
-        basepath = QPATHTOFOLDER(Templates\Vanilla);
-        file = "Vanilla_AI_AAF";
+    class RACS_Arid
+    {
+        requiredAddons[] = {"rhsgref_main"}; // to be done
+        logo = "a3\ui_f\data\logos\arma3_white_ca.paa"; // to be done
+        basepath = QPATHTOFOLDER(Templates\PRACS); 
+        priority = 80;
+        side = "Occ"; // RACS occ, SLA inv
+        flagTexture = "A3\Data_F\Flags\Flag_CSAT_CO.paa"; // to be added
+        name = "RACS Arid"; //the name shown in the selector
+        file = "RACS_AI_RACS_Arid"; //the template file name
+        maps[] = {"sara"}; 
+        climate[] = {"arid"}; 
+        // shortName = "CSAT"; // to be done
+        // lore = $STR_A3A_templates_lore_CSAT; // to be done
     };
 
-    class Vanilla_CSAT2 : Vanilla_Base
+    class RACS_Temperate : RACS_Arid
     {
-        basepath = QPATHTOFOLDER(Templates\Vanilla); //the path to the folder the template is located in, this translates to "\x\A3AE\addons\templates\Templates\Vanilla"
-        side = "Inv"; //the side the faction defaults to, one of the following: Inv, Occ, Reb, Civ
-        flagTexture = "A3\Data_F\Flags\Flag_CSAT_CO.paa"; //path to an icon to be displayed in the selector
-        name = "A3 CSAT 2"; //the name shown in the selector
-        file = "Vanilla_AI_CSAT2"; //the template file name
-        maps[] = {}; //if this template should be prioritized on any maps (case sensetive to worldName)
-        climate[] = {"arid", "arctic"}; //climate that the template is meant for
+        name = "RACS Temperate";
+        file = "RACS_AI_RACS_Temperate";
+        climate[] = {"temperate"};
+    };
+
+    class SLA_Arid : RACS_Arid
+    {
+        side = "Inv";
+        name = "SLA Arid";
+        file = "RACS_AI_SLA_Arid";
+    };
+
+    class SLA_Temperate : SLA_Arid
+    {
+        name = "SLA Temperate";
+        file = "RACS_AI_SLA_Temperate";
+        climate[] = {"temperate"};
+    };
+
+    class SLA_Rev_Guards : SLA_Arid
+    {
+        name = "SLA Revolutionary Guards";
+        file = "RACS_AI_SLA_Rev_Guards";
+        climate[] = {"temperate", "arid"};
     };
 };
