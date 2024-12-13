1e579ced74c70770c1722230a9d91a9a05ca8c52
Play3rgg <39414270+Play3rgg@users.noreply.github.com>
2024-12-13 10:33:13 +0200

    1st round of error fixing

--- a3ae_pracs/addons/functions/script_component.hpp
+++
@@ -1,2 +0,0 @@
-#define COMPONENT functions
-#include "\x\a3ae_pracs\addons\core\Includes\script_mod.hpp"
--- a3ae_pracs/addons/maps/script_component.hpp
+++
@@ -1,2 +0,0 @@
-#define COMPONENT maps
-#include "\x\a3ae_pracs\addons\core\Includes\script_mod.hpp"
