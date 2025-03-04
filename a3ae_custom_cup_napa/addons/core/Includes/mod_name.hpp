cd5e2566194c5b8da29250b820925de8cefb1a4e
Play3rgg <39414270+Play3rgg@users.noreply.github.com>
2025-03-04 16:32:30 +0200

    TKM and NAPA plane and AT car

---
+++ a3ae_custom_cup_napa/addons/core/Includes/mod_name.hpp
@@ -0,0 +1,4 @@
+#define MODFOLDER a3ae_custom_cup_napa
+#ifndef PREFIX
+    #define PREFIX a3ae_custom_cup_napa
+#endif
---
+++ a3ae_custom_cup_napa/addons/templates/Templates/Templates.hpp
@@ -0,0 +1,17 @@
+class Templates 
+{
+    class CUP_Reb  // overwrite existing template
+    {
+        basepath = QPATHTOFOLDER(Templates\CUP);
+        file = "CUP_Reb_NAPA";
+        name = "!! CUP NAPA";
+    };    
+
+    class CUP_TKM  // overwrite existing template
+    {
+        basepath = QPATHTOFOLDER(Templates\CUP);
+        file = "CUP_Reb_TKM";
+        name = "!! CUP TKM";
+    };    
+};
+
