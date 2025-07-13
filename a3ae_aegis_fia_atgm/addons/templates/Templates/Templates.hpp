c5c02dbfc439aa431ac6f076ce229f88a4e0e3e4
Play3rgg <39414270+Play3rgg@users.noreply.github.com>
2025-07-13 18:32:09 +0300

    c

--- a3ae_aegis_fia_atgm/addons/templates/Templates/Templates.hpp
+++ a3ae_aegis_fia_atgm/addons/templates/Templates/Templates.hpp
@@ -1,13 +1,14 @@
 class Templates {
-    class Aegis_Base;
-
-    class Aegis_FIA : Aegis_Base
+    
+    class Aegis_FIA
     {
-        side = "Reb";
-        flagTexture = "a3\data_f\flags\flag_fia_co.paa";
-        name = "Aegis FIA ATGM";
+        basepath = QPATHTOFOLDER(Templates\Aegis);
         file = "Aegis_Reb_FIA";
     };
 
 
 };
+
+
+
+
